790061e717169aa5ee1561478be9d2ea0aee8f02
Korng Samnang <korngsamnang168@gmail.com>
2025-04-26 10:00:00 +0700

    Update install the script

--- 02-install-cluster/setup/install-k8s-components.sh
+++ 02-install-cluster/setup/install-k8s-components.sh
@@ -1,18 +1,18 @@
 # Install packages needed to use the Kubernetes apt repository
 sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 
 # Download the Google Cloud public signing key
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
 # Add the Kubernetes apt repository
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 # Install kubelet, kubeadm & kubectl, and pin their versions
 sudo apt-get update
 # check available kubeadm versions (when manually executing)
 apt-cache madison kubeadm
 # Install version 1.21.0 for all components
-sudo apt-get install -y kubelet=1.21.0-00 kubeadm=1.21.0-00 kubectl=1.21.0-00
+sudo apt-get install -y kubelet=1.29.0-1.1 kubeadm=1.29.0-1.1 kubectl=1.29.0-1.1
 sudo apt-mark hold kubelet kubeadm kubectl
 ## apt-mark hold prevents package from being automatically upgraded or removed
