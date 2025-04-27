212b50f7afd3d2894029cbbf6316a28a1336cdfa
Korng Samnang <korngsamnang168@gmail.com>
2025-04-27 10:00:00 +0700

    Update the script

--- 02-install-cluster/setup/install-contrainerd.sh
+++ 02-install-cluster/setup/install-contrainerd.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-# sysctl params required by setup, params persist across reboots
+# Load required kernel modules
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# Sysctl params required by setup, persist across reboots
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-net.ipv4.ip_forward = 1
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
 EOF
 
 # Apply sysctl params without reboot
@@ -10,10 +21,12 @@ sudo sysctl --system
 
 # Install containerd
 sudo apt update
-sudo apt install containerd -y
+sudo apt install -y containerd
 
 # Configure containerd with defaults and restart with this config
 sudo mkdir -p /etc/containerd
 containerd config default | sudo tee /etc/containerd/config.toml
 sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+
 sudo systemctl restart containerd
+sudo systemctl enable containerd
